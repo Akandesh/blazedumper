af154f71740a6c389acc3f08e89f005cda7465bb
Akandesh <akandessh@live.com>
2020-07-10 00:02:56 +0200

    Update: Fri, Jul 10, 2020 12:02:56 AM

--- csgo.hpp
+++ csgo.hpp
@@ -1,10 +1,10 @@
 #pragma once
 #include <cstdint>
 
-// 09-07-2020 10:07:02
+// 10-07-2020 00:02:55
 
 namespace blazedumper {
-constexpr ::std::int64_t timestamp = 15942820226448968
+constexpr ::std::int64_t timestamp = 15943321754346306
 namespace netvars {
 constexpr ::std::ptrdiff_t cs_gamerules_data = 0x0;
 constexpr ::std::ptrdiff_t m_ArmorValue = 0xB378;
@@ -150,7 +150,7 @@ constexpr ::std::ptrdiff_t force_update_spectator_glow = 0x3A20E2;
 constexpr ::std::ptrdiff_t interface_engine_cvar = 0x3E9EC;
 constexpr ::std::ptrdiff_t is_c4_owner = 0x3AEB80;
 constexpr ::std::ptrdiff_t m_bDormant = 0xED;
-constexpr ::std::ptrdiff_t m_flSpawnTime = 0x0;
+constexpr ::std::ptrdiff_t m_flSpawnTime = 0xA370;
 constexpr ::std::ptrdiff_t m_pStudioHdr = 0x294C;
 constexpr ::std::ptrdiff_t m_pitchClassPtr = 0x51873F8;
 constexpr ::std::ptrdiff_t m_yawClassPtr = 0xD433F0;
