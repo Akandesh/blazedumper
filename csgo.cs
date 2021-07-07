974f9ee472101124f099473e1f77c9d1f43aeb4c
Akandesh <akandessh@live.com>
2021-07-07 21:27:26 +0200

    Update: Wed, Jul  7, 2021  9:27:26 PM

--- csgo.cs
+++ csgo.cs
@@ -1,8 +1,8 @@
 using System;
-// 07-07-2021 21:23:49
+// 07-07-2021 21:27:25
 
 namespace blazedumper {
-    public const Int32 timestamp = 16256858292094558
+    public const Int32 timestamp = 16256860453324737
     public static class netvars
     {
         public const Int32 cs_gamerules_data = 0x0;
@@ -150,7 +150,7 @@ namespace blazedumper {
         public const Int32 interface_engine_cvar = 0x3E9EC;
         public const Int32 is_c4_owner = 0x3BC2C0;
         public const Int32 m_bDormant = 0xED;
-        public const Int32 m_flSpawnTime = 0xA370;
+        public const Int32 m_flSpawnTime = 0x0;
         public const Int32 m_pStudioHdr = 0x294C;
         public const Int32 m_pitchClassPtr = 0x51D71C0;
         public const Int32 m_yawClassPtr = 0xD8EA78;
