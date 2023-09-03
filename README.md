d694ffac3167a953a053c7460e772e2c8238e9f0
Akandesh <Akandessh@live.com>
2023-09-03 19:37:40 +0200

    Update: Sun Sep  3 19:37:39 WEDT 2023

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 🚀 Up to date offsets for Counter-Strike: Global Offensive. Automatically kept up to date thanks to [csgo_auto_dumper](https://github.com/Akandesh/csgo_auto_dumper).
 
 
-### Last Update 30-06-2023 12:38:05
+### Last Update 03-09-2023 19:37:38
 ## Issues
  - None! :D
 
--- csgo.cs
+++ csgo.cs
@@ -1,5 +1,5 @@
 using System;
-// 30-06-2023 12:38:05
+// 03-09-2023 19:37:38
 
 namespace blazedumper {
     public static class netvars
@@ -114,47 +114,47 @@ namespace blazedumper {
         public const Int32 dwClientState_PlayerInfo = 0x52C0;
         public const Int32 dwClientState_State = 0x108;
         public const Int32 dwClientState_ViewAngles = 0x4D90;
-        public const Int32 dwEntityList = 0x4DFFF7C;
-        public const Int32 dwForceAttack = 0x322DDE8;
-        public const Int32 dwForceAttack2 = 0x322DDF4;
-        public const Int32 dwForceBackward = 0x322DE30;
-        public const Int32 dwForceForward = 0x322DE24;
-        public const Int32 dwForceJump = 0x52BBCD8;
-        public const Int32 dwForceLeft = 0x322DDA0;
-        public const Int32 dwForceRight = 0x322DDAC;
-        public const Int32 dwGameDir = 0x63AD80;
-        public const Int32 dwGameRulesProxy = 0x532F4E4;
-        public const Int32 dwGetAllClasses = 0xE0BF94;
+        public const Int32 dwEntityList = 0x4E0102C;
+        public const Int32 dwForceAttack = 0x322EE98;
+        public const Int32 dwForceAttack2 = 0x322EEA4;
+        public const Int32 dwForceBackward = 0x322EEE0;
+        public const Int32 dwForceForward = 0x322EED4;
+        public const Int32 dwForceJump = 0x52BCD88;
+        public const Int32 dwForceLeft = 0x322EE50;
+        public const Int32 dwForceRight = 0x322EE5C;
+        public const Int32 dwGameDir = 0x63AE00;
+        public const Int32 dwGameRulesProxy = 0x5330594;
+        public const Int32 dwGetAllClasses = 0xE0CFA4;
         public const Int32 dwGlobalVars = 0x59EE60;
-        public const Int32 dwGlowObjectManager = 0x535AA08;
-        public const Int32 dwInput = 0x525D550;
-        public const Int32 dwInterfaceLinkList = 0x99B4A4;
-        public const Int32 dwLocalPlayer = 0xDEA98C;
-        public const Int32 dwMouseEnable = 0x52391B0;
-        public const Int32 dwMouseEnablePtr = 0x5239180;
-        public const Int32 dwPlayerResource = 0x322C120;
-        public const Int32 dwRadarBase = 0x5236A54;
+        public const Int32 dwGlowObjectManager = 0x535BAD0;
+        public const Int32 dwInput = 0x525E600;
+        public const Int32 dwInterfaceLinkList = 0x99CF84;
+        public const Int32 dwLocalPlayer = 0xDEB99C;
+        public const Int32 dwMouseEnable = 0x523A260;
+        public const Int32 dwMouseEnablePtr = 0x523A230;
+        public const Int32 dwPlayerResource = 0x322D1D0;
+        public const Int32 dwRadarBase = 0x5237B04;
         public const Int32 dwSensitivity = 0x0;
         public const Int32 dwSensitivityPtr = 0x0;
-        public const Int32 dwSetClanTag = 0x8DA80;
-        public const Int32 dwViewMatrix = 0x4DF0DC4;
-        public const Int32 dwWeaponTable = 0x525E62C;
+        public const Int32 dwSetClanTag = 0x8DAB0;
+        public const Int32 dwViewMatrix = 0x4DF1E74;
+        public const Int32 dwWeaponTable = 0x525F6DC;
         public const Int32 dwWeaponTableIndex = 0x326C;
         public const Int32 dwYawPtr = 0x0;
         public const Int32 dwZoomSensitivityRatioPtr = 0x0;
-        public const Int32 dwbSendPackets = 0xDD2B2;
+        public const Int32 dwbSendPackets = 0xDD2D2;
         public const Int32 dwppDirect3DDevice9 = 0xA62C0;
-        public const Int32 find_hud_element = 0x6B2B02D0;
-        public const Int32 force_update_spectator_glow = 0x3D997A;
+        public const Int32 find_hud_element = 0x6C4C26C0;
+        public const Int32 force_update_spectator_glow = 0x3DB90A;
         public const Int32 interface_engine_cvar = 0x3FA9C;
-        public const Int32 is_c4_owner = 0x3E71A0;
+        public const Int32 is_c4_owner = 0x3E9120;
         public const Int32 m_bDormant = 0xED;
         public const Int32 m_flSpawnTime = 0x103C0;
         public const Int32 m_pStudioHdr = 0x2950;
-        public const Int32 m_pitchClassPtr = 0x52390A8;
+        public const Int32 m_pitchClassPtr = 0x523A158;
         public const Int32 m_yawClassPtr = 0x0;
         public const Int32 model_ambient_min = 0x5A1194;
-        public const Int32 set_abs_angles = 0x1E7A10;
-        public const Int32 set_abs_origin = 0x1E7850;
+        public const Int32 set_abs_angles = 0x1E8FC0;
+        public const Int32 set_abs_origin = 0x1E8E00;
     }
 } // namespace blazedumper
